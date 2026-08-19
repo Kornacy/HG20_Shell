eccb0d6e28caacc3dcea8c8f22d9eb3e91774337
Kamil <kamilbuszta.3@gmail.com>
2026-08-19 21:05:26 +0200

    feat: temporary responsive layout

--- gui/ui/ui_settings.py
+++ gui/ui/ui_settings.py
@@ -15,42 +15,87 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QSizePolicy,
-    QSpinBox, QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QPushButton,
+    QSizePolicy, QSpinBox, QTextEdit, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(400, 300)
-        self.acceptButton = QPushButton(Form)
-        self.acceptButton.setObjectName(u"acceptButton")
-        self.acceptButton.setGeometry(QRect(150, 200, 75, 24))
-        self.textEdit = QTextEdit(Form)
-        self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setGeometry(QRect(30, 90, 104, 71))
-        self.label = QLabel(Form)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(60, 70, 49, 16))
-        self.saveButton = QPushButton(Form)
-        self.saveButton.setObjectName(u"saveButton")
-        self.saveButton.setGeometry(QRect(150, 240, 75, 24))
-        self.heightSpinBox = QSpinBox(Form)
-        self.heightSpinBox.setObjectName(u"heightSpinBox")
-        self.heightSpinBox.setGeometry(QRect(251, 70, 81, 22))
-        self.heightSpinBox.setMinimum(640)
-        self.heightSpinBox.setMaximum(1080)
-        self.widthSpinBox = QSpinBox(Form)
-        self.widthSpinBox.setObjectName(u"widthSpinBox")
-        self.widthSpinBox.setGeometry(QRect(251, 130, 91, 22))
-        self.widthSpinBox.setMinimum(800)
-        self.widthSpinBox.setMaximum(1920)
+        Form.resize(400, 394)
+        self.gridLayout = QGridLayout(Form)
+        self.gridLayout.setObjectName(u"gridLayout")
         self.label_2 = QLabel(Form)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(248, 50, 61, 20))
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
+
+        self.heightSpinBox = QSpinBox(Form)
+        self.heightSpinBox.setObjectName(u"heightSpinBox")
+        sizePolicy.setHeightForWidth(self.heightSpinBox.sizePolicy().hasHeightForWidth())
+        self.heightSpinBox.setSizePolicy(sizePolicy)
+        self.heightSpinBox.setMinimumSize(QSize(0, 25))
+        self.heightSpinBox.setMinimum(640)
+        self.heightSpinBox.setMaximum(1080)
+
+        self.gridLayout.addWidget(self.heightSpinBox, 1, 0, 1, 1)
+
         self.label_3 = QLabel(Form)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(248, 110, 71, 20))
+        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy)
+        self.label_3.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.widthSpinBox = QSpinBox(Form)
+        self.widthSpinBox.setObjectName(u"widthSpinBox")
+        sizePolicy.setHeightForWidth(self.widthSpinBox.sizePolicy().hasHeightForWidth())
+        self.widthSpinBox.setSizePolicy(sizePolicy)
+        self.widthSpinBox.setMinimumSize(QSize(0, 25))
+        self.widthSpinBox.setMinimum(800)
+        self.widthSpinBox.setMaximum(1920)
+
+        self.gridLayout.addWidget(self.widthSpinBox, 3, 0, 1, 1)
+
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.label, 4, 0, 1, 1)
+
+        self.textEdit = QTextEdit(Form)
+        self.textEdit.setObjectName(u"textEdit")
+        sizePolicy.setHeightForWidth(self.textEdit.sizePolicy().hasHeightForWidth())
+        self.textEdit.setSizePolicy(sizePolicy)
+        self.textEdit.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.textEdit, 5, 0, 1, 1)
+
+        self.acceptButton = QPushButton(Form)
+        self.acceptButton.setObjectName(u"acceptButton")
+        sizePolicy.setHeightForWidth(self.acceptButton.sizePolicy().hasHeightForWidth())
+        self.acceptButton.setSizePolicy(sizePolicy)
+        self.acceptButton.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.acceptButton, 6, 0, 1, 1)
+
+        self.saveButton = QPushButton(Form)
+        self.saveButton.setObjectName(u"saveButton")
+        sizePolicy.setHeightForWidth(self.saveButton.sizePolicy().hasHeightForWidth())
+        self.saveButton.setSizePolicy(sizePolicy)
+        self.saveButton.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.saveButton, 7, 0, 1, 1)
+
 
         self.retranslateUi(Form)
 
@@ -59,10 +104,10 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.acceptButton.setText(QCoreApplication.translate("Form", u"Accept", None))
-        self.label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
-        self.saveButton.setText(QCoreApplication.translate("Form", u"Save", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"Wysokosc", None))
         self.label_3.setText(QCoreApplication.translate("Form", u"Szerokosc", None))
+        self.label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
+        self.acceptButton.setText(QCoreApplication.translate("Form", u"Accept", None))
+        self.saveButton.setText(QCoreApplication.translate("Form", u"Save", None))
     # retranslateUi
 
--- gui/ui/ui_shell.py
+++ gui/ui/ui_shell.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
-    QSizePolicy, QStackedWidget, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QStackedWidget, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -25,26 +26,61 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.backButton = QPushButton(self.centralwidget)
+        self.backButton.setObjectName(u"backButton")
+        self.backButton.setEnabled(True)
+
+        self.horizontalLayout.addWidget(self.backButton)
+
+        self.screenTitle = QLabel(self.centralwidget)
+        self.screenTitle.setObjectName(u"screenTitle")
+        self.screenTitle.setEnabled(True)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.screenTitle.sizePolicy().hasHeightForWidth())
+        self.screenTitle.setSizePolicy(sizePolicy)
+        self.screenTitle.setLocale(QLocale(QLocale.Polish, QLocale.Poland))
+        self.screenTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.horizontalLayout.addWidget(self.screenTitle)
+
+        self.settingsButton = QPushButton(self.centralwidget)
+        self.settingsButton.setObjectName(u"settingsButton")
+        self.settingsButton.setEnabled(True)
+
+        self.horizontalLayout.addWidget(self.settingsButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
         self.stackedWidget = QStackedWidget(self.centralwidget)
         self.stackedWidget.setObjectName(u"stackedWidget")
-        self.stackedWidget.setGeometry(QRect(-1, 49, 801, 551))
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy1.setHorizontalStretch(1)
+        sizePolicy1.setVerticalStretch(1)
+        sizePolicy1.setHeightForWidth(self.stackedWidget.sizePolicy().hasHeightForWidth())
+        self.stackedWidget.setSizePolicy(sizePolicy1)
         self.page = QWidget()
         self.page.setObjectName(u"page")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        sizePolicy2.setHorizontalStretch(1)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.page.sizePolicy().hasHeightForWidth())
+        self.page.setSizePolicy(sizePolicy2)
         self.stackedWidget.addWidget(self.page)
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
+        sizePolicy2.setHeightForWidth(self.page_2.sizePolicy().hasHeightForWidth())
+        self.page_2.setSizePolicy(sizePolicy2)
         self.stackedWidget.addWidget(self.page_2)
-        self.backButton = QPushButton(self.centralwidget)
-        self.backButton.setObjectName(u"backButton")
-        self.backButton.setGeometry(QRect(10, 10, 75, 24))
-        self.settingsButton = QPushButton(self.centralwidget)
-        self.settingsButton.setObjectName(u"settingsButton")
-        self.settingsButton.setGeometry(QRect(710, 10, 75, 24))
-        self.screenTitle = QLabel(self.centralwidget)
-        self.screenTitle.setObjectName(u"screenTitle")
-        self.screenTitle.setGeometry(QRect(360, 10, 91, 21))
-        self.screenTitle.setLocale(QLocale(QLocale.Polish, QLocale.Poland))
-        self.screenTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout.addWidget(self.stackedWidget)
+
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -54,8 +90,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.backButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.settingsButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.backButton.setText(QCoreApplication.translate("MainWindow", u"Back", None))
         self.screenTitle.setText(QCoreApplication.translate("MainWindow", u"HandGame 2.0", None))
+        self.settingsButton.setText(QCoreApplication.translate("MainWindow", u"Settings", None))
     # retranslateUi
 
