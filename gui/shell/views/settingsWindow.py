54016df69595f9aaeb05c5314ad3b7f5f3be31c5
Kamil <kamilbuszta.3@gmail.com>
2026-08-20 22:18:27 +0200

    feat: combobox resolution

--- gui/shell/views/settingsWindow.py
+++ gui/shell/views/settingsWindow.py
@@ -2,7 +2,13 @@ from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QWidget, QPushButton, QSpinBox
 from ui.ui_settings import Ui_Form
 
-
+RESOLUTIONS = [
+    (800, 600),
+    (1024, 768),
+    (1280, 720),
+    (1600, 900),
+    (1920, 1080),
+]
 class SettingWindow(QWidget):
     resolutionChange = Signal(int,int)
 
@@ -11,11 +17,18 @@ class SettingWindow(QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
+        for w, h in RESOLUTIONS:
+            self.ui.resComboBox.addItem(f"{w} x {h}",(w,h))
+        
         self.ui.saveButton.clicked.connect(self.eimtResolution)
 
     def setResolution(self, width: int, height: int) -> None:
-        self.ui.widthSpinBox.setValue(width)
-        self.ui.heightSpinBox.setValue(height)
-
+        target = (width, height)
+        for i in range(self.ui.resComboBox.count()):
+            if self.ui.resComboBox.itemData(i) == target:
+                self.ui.resComboBox.setCurrentIndex(i)
+                return
+        self.ui.resComboBox.setCurrentIndex(0)
     def eimtResolution(self) -> None:
-        self.resolutionChange.emit(self.ui.widthSpinBox.value(),self.ui.heightSpinBox.value())
+        w, h = self.ui.resComboBox.currentData()
+        self.resolutionChange.emit(w,h)
--- gui/ui/ui_settings.py
+++ gui/ui/ui_settings.py
@@ -15,8 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QPushButton,
-    QSizePolicy, QSpinBox, QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
+    QPushButton, QSizePolicy, QTextEdit, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -25,60 +25,21 @@ class Ui_Form(object):
         Form.resize(400, 394)
         self.gridLayout = QGridLayout(Form)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label_2 = QLabel(Form)
-        self.label_2.setObjectName(u"label_2")
+        self.textEdit = QTextEdit(Form)
+        self.textEdit.setObjectName(u"textEdit")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
-        self.label_2.setSizePolicy(sizePolicy)
-        self.label_2.setMinimumSize(QSize(0, 25))
-
-        self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
-
-        self.heightSpinBox = QSpinBox(Form)
-        self.heightSpinBox.setObjectName(u"heightSpinBox")
-        sizePolicy.setHeightForWidth(self.heightSpinBox.sizePolicy().hasHeightForWidth())
-        self.heightSpinBox.setSizePolicy(sizePolicy)
-        self.heightSpinBox.setMinimumSize(QSize(0, 25))
-        self.heightSpinBox.setMinimum(640)
-        self.heightSpinBox.setMaximum(1080)
-
-        self.gridLayout.addWidget(self.heightSpinBox, 1, 0, 1, 1)
-
-        self.label_3 = QLabel(Form)
-        self.label_3.setObjectName(u"label_3")
-        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
-        self.label_3.setSizePolicy(sizePolicy)
-        self.label_3.setMinimumSize(QSize(0, 25))
-
-        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
-
-        self.widthSpinBox = QSpinBox(Form)
-        self.widthSpinBox.setObjectName(u"widthSpinBox")
-        sizePolicy.setHeightForWidth(self.widthSpinBox.sizePolicy().hasHeightForWidth())
-        self.widthSpinBox.setSizePolicy(sizePolicy)
-        self.widthSpinBox.setMinimumSize(QSize(0, 25))
-        self.widthSpinBox.setMinimum(800)
-        self.widthSpinBox.setMaximum(1920)
-
-        self.gridLayout.addWidget(self.widthSpinBox, 3, 0, 1, 1)
-
-        self.label = QLabel(Form)
-        self.label.setObjectName(u"label")
-        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
-        self.label.setSizePolicy(sizePolicy)
-        self.label.setMinimumSize(QSize(0, 25))
-
-        self.gridLayout.addWidget(self.label, 4, 0, 1, 1)
-
-        self.textEdit = QTextEdit(Form)
-        self.textEdit.setObjectName(u"textEdit")
         sizePolicy.setHeightForWidth(self.textEdit.sizePolicy().hasHeightForWidth())
         self.textEdit.setSizePolicy(sizePolicy)
         self.textEdit.setMinimumSize(QSize(0, 25))
 
-        self.gridLayout.addWidget(self.textEdit, 5, 0, 1, 1)
+        self.gridLayout.addWidget(self.textEdit, 3, 0, 1, 1)
+
+        self.resComboBox = QComboBox(Form)
+        self.resComboBox.setObjectName(u"resComboBox")
+
+        self.gridLayout.addWidget(self.resComboBox, 1, 0, 1, 1)
 
         self.acceptButton = QPushButton(Form)
         self.acceptButton.setObjectName(u"acceptButton")
@@ -86,7 +47,7 @@ class Ui_Form(object):
         self.acceptButton.setSizePolicy(sizePolicy)
         self.acceptButton.setMinimumSize(QSize(0, 25))
 
-        self.gridLayout.addWidget(self.acceptButton, 6, 0, 1, 1)
+        self.gridLayout.addWidget(self.acceptButton, 4, 0, 1, 1)
 
         self.saveButton = QPushButton(Form)
         self.saveButton.setObjectName(u"saveButton")
@@ -94,7 +55,23 @@ class Ui_Form(object):
         self.saveButton.setSizePolicy(sizePolicy)
         self.saveButton.setMinimumSize(QSize(0, 25))
 
-        self.gridLayout.addWidget(self.saveButton, 7, 0, 1, 1)
+        self.gridLayout.addWidget(self.saveButton, 5, 0, 1, 1)
+
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
+
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setMinimumSize(QSize(0, 25))
+
+        self.gridLayout.addWidget(self.label, 2, 0, 1, 1)
 
 
         self.retranslateUi(Form)
@@ -104,10 +81,9 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.label_2.setText(QCoreApplication.translate("Form", u"Wysokosc", None))
-        self.label_3.setText(QCoreApplication.translate("Form", u"Szerokosc", None))
-        self.label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
         self.acceptButton.setText(QCoreApplication.translate("Form", u"Accept", None))
         self.saveButton.setText(QCoreApplication.translate("Form", u"Save", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"Rozdzielczo\u015b\u0107", None))
+        self.label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
     # retranslateUi
 
