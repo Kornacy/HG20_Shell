2979e3469e3758b7d7b62af41c2a9d1bd709243e
Kamil <kamilbuszta.3@gmail.com>
2026-08-20 18:31:37 +0200

    feat: dynamic title

--- gui/shell/shellWindow.py
+++ gui/shell/shellWindow.py
@@ -9,20 +9,23 @@ class ShellWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self._pages: dict[str, QWidget] = {}
+        self._pages: dict[str, tuple[QWidget, str]] = {}
         self.settingPage = SettingWindow()   
-        self.addPage("settings",self.settingPage)
-        self.addPage("main",StartPage())
+        self.addPage("settings",self.settingPage,"Ustawienia")
+        self.addPage("main",StartPage(),"HandGame 2.0")
         self.openPage("main")
         self.settingPage.resolutionChange.connect(self.applyResolution)
         self.settingPage.setResolution(self.width(), self.height())
         self.ui.backButton.clicked.connect(lambda: self.openPage("main"))
         self.ui.settingsButton.clicked.connect(lambda: self.openPage("settings"))
-    def addPage(self, name: str, page: QWidget) -> None:
-        self._pages[name] = page
+
+    def addPage(self, id: str, page: QWidget, title: str) -> None:
+        self._pages[id] = (page, title)
         self.ui.stackedWidget.addWidget(page)
-    def openPage(self, name: str) -> None:
-        page = self._pages[name]
+
+    def openPage(self, id: str) -> None:
+        page, title = self._pages[id]
+        self.ui.screenTitle.setText(title)
         self.ui.stackedWidget.setCurrentWidget(page)
 
     def applyResolution(self, width: int, height: int) -> None:
