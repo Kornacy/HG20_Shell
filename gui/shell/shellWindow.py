169632e1273e3cc84cc192d52c57787e138d318b
Kamil <kamilbuszta.3@gmail.com>
2026-08-20 19:47:48 +0200

    feat: add back button

--- gui/shell/shellWindow.py
+++ gui/shell/shellWindow.py
@@ -11,12 +11,21 @@ class ShellWindow(QMainWindow):
 
         self._pages: dict[str, tuple[QWidget, str]] = {}
         self.settingPage = SettingWindow()   
+        self._history: list[str] = []
+        self._currentPage: str | None = None
+
+        #Dodawanie ekranów na shell
         self.addPage("settings",self.settingPage,"Ustawienia")
         self.addPage("main",StartPage(),"HandGame 2.0")
+
+        #Ekran startowy
         self.openPage("main")
+
         self.settingPage.resolutionChange.connect(self.applyResolution)
         self.settingPage.setResolution(self.width(), self.height())
-        self.ui.backButton.clicked.connect(lambda: self.openPage("main"))
+
+        #Przyciski
+        self.ui.backButton.clicked.connect(lambda: self.goBack())
         self.ui.settingsButton.clicked.connect(lambda: self.openPage("settings"))
 
     def addPage(self, id: str, page: QWidget, title: str) -> None:
@@ -24,9 +33,25 @@ class ShellWindow(QMainWindow):
         self.ui.stackedWidget.addWidget(page)
 
     def openPage(self, id: str) -> None:
+        if id not in self._pages:
+            return
+        if id == self._currentPage:
+            return
+        if self._currentPage is not None:
+            self._history.append(self._currentPage)
+        self._currentPage = id
         page, title = self._pages[id]
         self.ui.screenTitle.setText(title)
         self.ui.stackedWidget.setCurrentWidget(page)
 
     def applyResolution(self, width: int, height: int) -> None:
         self.resize(width, height)
+
+    def goBack(self) -> None:
+        if not self._history:
+            return
+        prevId = self._history.pop()
+        self._currentPage = prevId
+        page, title = self._pages[prevId]
+        self.ui.screenTitle.setText(title)
+        self.ui.stackedWidget.setCurrentWidget(page)
