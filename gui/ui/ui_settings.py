f94a7bfa2e87abc1b9b84eaf8724b553168826ad
Kamil <kamilbuszta.3@gmail.com>
2026-08-28 23:21:18 +0200

    feat: add __init__.py

--- gui/ui/ui_settings.py
+++ gui/ui/ui_settings.py
@@ -26,12 +26,6 @@ class Ui_Form(object):
         Form.resize(400, 394)
         self.gridLayout = QGridLayout(Form)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.fullScreenCheckBox = QCheckBox(Form)
-        self.fullScreenCheckBox.setObjectName(u"fullScreenCheckBox")
-        self.fullScreenCheckBox.setLocale(QLocale(QLocale.Polish, QLocale.Poland))
-
-        self.gridLayout.addWidget(self.fullScreenCheckBox, 2, 0, 1, 1)
-
         self.saveButton = QPushButton(Form)
         self.saveButton.setObjectName(u"saveButton")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
@@ -41,20 +35,7 @@ class Ui_Form(object):
         self.saveButton.setSizePolicy(sizePolicy)
         self.saveButton.setMinimumSize(QSize(0, 25))
 
-        self.gridLayout.addWidget(self.saveButton, 6, 0, 1, 1)
-
-        self.acceptButton = QPushButton(Form)
-        self.acceptButton.setObjectName(u"acceptButton")
-        sizePolicy.setHeightForWidth(self.acceptButton.sizePolicy().hasHeightForWidth())
-        self.acceptButton.setSizePolicy(sizePolicy)
-        self.acceptButton.setMinimumSize(QSize(0, 25))
-
-        self.gridLayout.addWidget(self.acceptButton, 5, 0, 1, 1)
-
-        self.resComboBox = QComboBox(Form)
-        self.resComboBox.setObjectName(u"resComboBox")
-
-        self.gridLayout.addWidget(self.resComboBox, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.saveButton, 5, 0, 1, 1)
 
         self.label_2 = QLabel(Form)
         self.label_2.setObjectName(u"label_2")
@@ -64,9 +45,20 @@ class Ui_Form(object):
 
         self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
 
+        self.resComboBox = QComboBox(Form)
+        self.resComboBox.setObjectName(u"resComboBox")
+
+        self.gridLayout.addWidget(self.resComboBox, 1, 0, 1, 1)
+
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
         self.gridLayout.addItem(self.verticalSpacer, 3, 0, 1, 1)
+
+        self.fullScreenCheckBox = QCheckBox(Form)
+        self.fullScreenCheckBox.setObjectName(u"fullScreenCheckBox")
+        self.fullScreenCheckBox.setLocale(QLocale(QLocale.Polish, QLocale.Poland))
+
+        self.gridLayout.addWidget(self.fullScreenCheckBox, 2, 0, 1, 1)
 
 
         self.retranslateUi(Form)
@@ -76,9 +68,8 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.fullScreenCheckBox.setText(QCoreApplication.translate("Form", u"Pe\u0142ny Ekran", None))
         self.saveButton.setText(QCoreApplication.translate("Form", u"Save", None))
-        self.acceptButton.setText(QCoreApplication.translate("Form", u"Accept", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"Rozdzielczo\u015b\u0107", None))
+        self.fullScreenCheckBox.setText(QCoreApplication.translate("Form", u"Pe\u0142ny Ekran", None))
     # retranslateUi
 
